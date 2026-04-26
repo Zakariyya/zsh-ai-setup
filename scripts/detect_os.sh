11018b79a5ab5012584b52f92234373d3a47bac9
anan <rabbittucute@gmail.com>
2026-04-26 16:02:39 +0800

    进行部分补充调整-1

--- scripts/detect_os.sh
+++ scripts/detect_os.sh
@@ -2,8 +2,35 @@
 
 OS_ID=""
 OS_ID_LIKE=""
+OS_NAME=""
+OS_PRETTY_NAME=""
 IS_WSL="no"
 PKG_MGR=""
+
+is_debian_family_os() {
+  local id="${OS_ID,,}"
+  local like="${OS_ID_LIKE,,}"
+  local name="${OS_NAME,,}"
+  local pretty="${OS_PRETTY_NAME,,}"
+
+  if [[ "$id" =~ ^(ubuntu|debian|deepin|linuxdeepin|uos)$ ]]; then
+    return 0
+  fi
+
+  if [[ "$like" =~ (^|[[:space:]])(ubuntu|debian)([[:space:]]|$) ]]; then
+    return 0
+  fi
+
+  if [[ "$name" == *deepin* || "$pretty" == *deepin* || "$name" == *uos* || "$pretty" == *uos* ]]; then
+    return 0
+  fi
+
+  if [[ -f /etc/debian_version ]]; then
+    return 0
+  fi
+
+  return 1
+}
 
 detect_os() {
   if [[ -f /etc/os-release ]]; then
@@ -11,16 +38,20 @@ detect_os() {
     source /etc/os-release
     OS_ID="${ID:-unknown}"
     OS_ID_LIKE="${ID_LIKE:-}"
+    OS_NAME="${NAME:-}"
+    OS_PRETTY_NAME="${PRETTY_NAME:-}"
   else
     OS_ID="unknown"
     OS_ID_LIKE=""
+    OS_NAME=""
+    OS_PRETTY_NAME=""
   fi
 
   if grep -qiE '(microsoft|wsl)' /proc/version 2>/dev/null; then
     IS_WSL="yes"
   fi
 
-  if [[ "$OS_ID" =~ ^(ubuntu|debian|deepin)$ ]] || [[ "$OS_ID_LIKE" =~ (^|[[:space:]])(ubuntu|debian)([[:space:]]|$) ]]; then
+  if is_debian_family_os; then
     PKG_MGR="apt"
   else
     PKG_MGR=""
@@ -31,7 +62,7 @@ is_supported_platform() {
   if [[ "$IS_WSL" == "yes" ]]; then
     return 0
   fi
-  if [[ "$OS_ID" =~ ^(ubuntu|debian|deepin)$ ]] || [[ "$OS_ID_LIKE" =~ (^|[[:space:]])(ubuntu|debian)([[:space:]]|$) ]]; then
+  if is_debian_family_os; then
     return 0
   fi
   return 1
